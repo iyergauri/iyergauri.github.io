7d8787b646b116e84f942b4e6e80b08ef0058ba8
Gauri Iyer <iyergauri@gmail.com>
2022-05-13 02:37:27 -0700

    burn it

--- src/main.js
+++
@@ -1,9 +0,0 @@
-function kachow() {
-	var logo = document.getElementById("logo");
-	const red = Math.trunc(256 * Math.random());
-	const green = Math.trunc(256 * Math.random());
-	const blue = Math.trunc(256 * Math.random());
-	var newCol = `rgb(${red}, ${green}, ${blue})`;
-	console.log(newCol);
-	logo.style.color = newCol;
-}
