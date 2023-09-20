88ad76ef155c693a10c895b1bdc90b2fa2dc7712
gauri <iyergauri@gmail.com>
2023-09-20 16:33:02 -0700

    new job in html, fix redundant css, add logo click to landing page

--- function.js
+++ function.js
@@ -1,31 +1,33 @@
-
+function hideAll() {
+  ($('.summary').addClass("hide") && $('.button').removeClass('on'));
+}
 
 function toggleText(selector) {
- const category = `#${selector}`;
- const svgButtonOn = $(category).hasClass("on");
- ($('.summary').addClass("hide") && $('.button').removeClass('on'));
- if (!svgButtonOn) {
-   $(category).addClass("on");
-  $(`${category}text`).removeClass('hide');
- }
+  const category = `#${selector}`;
+  const svgButtonOn = $(category).hasClass("on");
+  hideAll();
+  if (!svgButtonOn) {
+    $(category).addClass("on");
+    $(`${category}text`).removeClass('hide');
+  }
 }
 
 $(window).on('load', () => {
 
- $('.button').hover(() => {
-  $(this).css({ cursor: 'pointer' });
- });
+  $('#logo').on('click', () => {
+    hideAll();
+  })
 
- $('#cs').on('click', () => {
-  toggleText('cs');
- })
+  $('#cs').on('click', () => {
+    toggleText('cs');
+  })
 
- $('#music').on('click', () => {
-  toggleText('music');
- })
+  $('#music').on('click', () => {
+    toggleText('music');
+  })
 
- $('#yoga').on('click', () => {
-  toggleText('yoga');
- })
+  $('#yoga').on('click', () => {
+    toggleText('yoga');
+  })
 
 });
